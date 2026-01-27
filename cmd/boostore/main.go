3dbaa843f505c714048ddbc637eed6fae684cabc
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-27 17:26:10 +0500

    chore: ping db

--- cmd/boostore/main.go
+++ cmd/boostore/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+
+	"github.com/BlackHole55/software-store-final/config"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config.ConnectDB()
+}
