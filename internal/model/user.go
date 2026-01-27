7d03e8b6cd6a0764b3e3b4d9298f8a117404e527
Animantra <t20227893@gmail.com>
2026-01-27 21:58:05 +0500

    feat: user model

---
+++ internal/model/user.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type User struct {
+	ID         bson.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Username   string        `bson:"username" json:"username"`
+	Email      string        `bson:"email" json:"email"`
+	Password   []byte        `bson:"password_hash" json:"-"`
+	Role       string        `bson:"role,omitempty" json:"role"`
+	Created_at time.Time     `bson:"created_at" json:"created_at"`
+	Updated_at time.Time     `bson:"updated_at" json:"updated_at"`
+}
