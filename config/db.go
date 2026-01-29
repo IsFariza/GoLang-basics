f09207f00fc3ec30756945807d98305176f85719
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-29 17:43:15 +0500

    feat: configure db connection

--- config/db.go
+++ config/db.go
@@ -4,32 +4,35 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
-func ConnectDB() {
+var DB *mongo.Client
+
+func ConnectDB() *mongo.Client {
 	mongoURI := os.Getenv("MONGODB_URI")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
+	defer cancel()
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
 
 	client, err := mongo.Connect(opts)
 	if err != nil {
-		panic(err)
+		panic(fmt.Sprintf("Failed to initialize MongoDB client: %v", err))
 	}
 
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		panic(err)
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(fmt.Sprintf("Failed to ping MongoDB: %v", err))
 	}
 
 	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	DB = client
+	return client
 }
