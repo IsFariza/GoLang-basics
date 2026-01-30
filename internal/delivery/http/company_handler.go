fdb700e025347bc809ea222bc7e575767c8e9262
Animantra <t20227893@gmail.com>
2026-01-30 22:51:38 +0500

    feat: add company handler

---
+++ internal/delivery/http/company_handler.go
@@ -0,0 +1,88 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/BlackHole55/software-store-final/internal/domain"
+	"github.com/BlackHole55/software-store-final/internal/usecase"
+	"github.com/gin-gonic/gin"
+)
+
+type CompanyHandler struct {
+	usecase *usecase.CompanyUsecase
+}
+
+func NewCompanyHandler(usecase *usecase.CompanyUsecase) *CompanyHandler {
+	return &CompanyHandler{
+		usecase: usecase,
+	}
+}
+
+func (h *CompanyHandler) Create(c *gin.Context) {
+	var company domain.Company
+
+	if err := c.ShouldBindJSON(&company); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx := c.Request.Context()
+
+	h.usecase.Create(ctx, company)
+
+}
+
+func (h *CompanyHandler) GetAll(c *gin.Context) {
+	ctx := c.Request.Context()
+	companies, err := h.usecase.GetAll(ctx)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, companies)
+}
+
+func (h *CompanyHandler) GetById(c *gin.Context) {
+	ctx := c.Request.Context()
+	id := c.Param("id")
+	companies, err := h.usecase.GetById(ctx, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, companies)
+}
+
+func (h *CompanyHandler) Update(c *gin.Context) {
+	ctx := c.Request.Context()
+	id := c.Param("id")
+
+	var updates domain.Company
+	if err := c.ShouldBindJSON(&updates); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := h.usecase.Update(ctx, id, updates)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "company updated"})
+}
+
+func (h *CompanyHandler) Delete(c *gin.Context) {
+	ctx := c.Request.Context()
+	id := c.Param("id")
+	err := h.usecase.Delete(ctx, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	//todo check doc id exist
+
+	c.JSON(http.StatusOK, gin.H{"message": "company deleted"})
+}
