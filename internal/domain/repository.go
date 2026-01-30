92209e1a7beb460006c7e6a99df4c93c5ca66924
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-30 22:42:38 +0500

    feat: add repository interface

--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -4,6 +4,14 @@ import (
 	"context"
 )
 
+type GameRepo interface {
+	Create(ctx context.Context, game Game) error
+	GetAll(ctx context.Context) ([]Game, error)
+	GetById(ctx context.Context, id string) (Game, error)
+	Update(ctx context.Context, id string, updates Game) error
+	Delete(ctx context.Context, id string) error
+}
+
 type CompanyRepo interface {
 	Create(ctx context.Context, company Company) error
 	GetAll(ctx context.Context) ([]Company, error)
