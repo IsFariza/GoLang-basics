ff9b81a72a9683877568be2706c2e895fc46c6ce
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-30 22:44:33 +0500

    feat: add game handler

---
+++ internal/delivery/http/game_handler.go
@@ -0,0 +1,91 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/BlackHole55/software-store-final/internal/domain"
+	"github.com/BlackHole55/software-store-final/internal/usecase"
+	"github.com/gin-gonic/gin"
+)
+
+type GameHandler struct {
+	usecase *usecase.GameUseCase
+}
+
+func NewGameHandler(usecase *usecase.GameUseCase) *GameHandler {
+	return &GameHandler{
+		usecase: usecase,
+	}
+}
+
+// POST api/v1/games
+func (h *GameHandler) CreateGame(c *gin.Context) {
+	var game domain.Game
+	if err := c.ShouldBindJSON(&game); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := h.usecase.CreateGame(c.Request.Context(), game)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Game created successfully"})
+}
+
+// GET api/v1/games
+func (h *GameHandler) GetAll(c *gin.Context) {
+	games, err := h.usecase.GetAll(c.Request.Context())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, games)
+}
+
+// GET api/v1/games/:id
+func (h *GameHandler) GetById(c *gin.Context) {
+	id := c.Param("id")
+	task, err := h.usecase.GetById(c.Request.Context(), id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, task)
+}
+
+// PUT api/v1/games/:id
+func (h *GameHandler) Update(c *gin.Context) {
+	id := c.Param("id")
+	var game domain.Game
+
+	if err := c.ShouldBindJSON(&game); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := h.usecase.Update(c.Request.Context(), id, game)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Game updated successfully"})
+}
+
+// DELETE api/v1/games/:id
+func (h *GameHandler) Delete(c *gin.Context) {
+	id := c.Param("id")
+
+	err := h.usecase.Delete(c.Request.Context(), id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Game deleted"})
+}
