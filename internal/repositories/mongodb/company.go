7e5710d0202c642790c94cbb35c059de2ce4d4d1
Animantra <t20227893@gmail.com>
2026-01-30 22:52:00 +0500

    feat: add company repository

---
+++ internal/repositories/mongodb/company.go
@@ -0,0 +1,97 @@
+package mongodb
+
+import (
+	"context"
+	"errors"
+
+	"github.com/BlackHole55/software-store-final/internal/domain"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type CompanyRepository struct {
+	collection *mongo.Collection
+}
+
+func NewCompanyRepository(client *mongo.Client) *CompanyRepository {
+	return &CompanyRepository{
+		collection: client.Database("softwarestore").Collection("companies"),
+	}
+}
+
+func (r *CompanyRepository) Create(ctx context.Context, company domain.Company) error {
+
+	_, err := r.collection.InsertOne(ctx, company)
+
+	return err
+}
+
+func (r *CompanyRepository) GetAll(ctx context.Context) ([]domain.Company, error) {
+	var companies []domain.Company
+
+	cursor, err := r.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	err = cursor.All(ctx, &companies)
+
+	return companies, err
+}
+
+func (r *CompanyRepository) GetById(ctx context.Context, id string) (domain.Company, error) {
+	var company domain.Company
+
+	objID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return domain.Company{}, err
+	}
+
+	filter := bson.M{"_id": objID}
+
+	err = r.collection.FindOne(ctx, filter).Decode(&company)
+
+	return company, err
+
+}
+
+func (r *CompanyRepository) Update(ctx context.Context, id string, updates domain.Company) error {
+	objID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": objID}
+	update := bson.M{
+		"$set": bson.M{
+			"name":        updates.Name,
+			"description": updates.Description,
+			"country":     updates.Country,
+			"contacts":    updates.Contacts,
+			"created_at":  updates.CreatedAt,
+			"updated_at":  updates.UpdatedAt,
+		},
+	}
+
+	_, err = r.collection.UpdateOne(ctx, filter, update)
+	return err
+}
+
+func (r *CompanyRepository) Delete(ctx context.Context, id string) error {
+	objID, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": objID}
+
+	res, err := r.collection.DeleteOne(ctx, filter)
+
+	//todo fix
+	if res.DeletedCount == 0 {
+		return errors.New("company not found")
+	}
+
+	return err
+}
