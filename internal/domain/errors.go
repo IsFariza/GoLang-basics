0ce63398ed612962ae32dc421ce6f85055374e46
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-31 15:32:20 +0500

    feat: add invalid company error

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,3 +3,5 @@ package domain
 import "errors"
 
 var ErrorNotFound = errors.New("resource not found")
+var ErrorInvalidPublisher = errors.New("Invalid publisher: does not exist")
+var ErrorInvalidDeveloper = errors.New("Invalid developer: does not exist")
