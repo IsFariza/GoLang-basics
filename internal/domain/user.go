13e2a93b3a85f8368a2c722bff8e3ae8374c0131
Animantra <t20227893@gmail.com>
2026-01-31 22:47:01 +0500

    feat: add rquired fields

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -8,10 +8,10 @@ import (
 
 type User struct {
 	ID        bson.ObjectID `bson:"_id,omitempty" json:"_id"`
-	Username  string        `bson:"username" json:"username"`
-	Email     string        `bson:"email" json:"email"`
-	Password  []byte        `bson:"password_hash" json:"-"`
+	Username  string        `bson:"username" json:"username" binding:"required"`
+	Email     string        `bson:"email" json:"email" binding:"required"`
+	Password  []byte        `bson:"password_hash" json:"-" binding:"required"`
 	Role      string        `bson:"role,omitempty" json:"role"`
 	CreatedAt time.Time     `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time     `bson:"updated_at" json:"updated_at"`
+	UpdatedAt *time.Time    `bson:"updated_at" json:"updated_at"`
 }
