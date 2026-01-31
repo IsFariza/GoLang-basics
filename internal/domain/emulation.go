0e7b90675a5ee10c83f9e2d2e33ea93261f857df
Animantra <t20227893@gmail.com>
2026-01-31 15:56:06 +0500

    fix: change required fields

--- internal/domain/emulation.go
+++ internal/domain/emulation.go
@@ -3,6 +3,6 @@ package domain
 import "go.mongodb.org/mongo-driver/v2/bson"
 
 type Emulation struct {
-	ID   bson.ObjectID `bson:"_id,omitempty" json:"_id" binding:"required"`
-	Name string        `bson:"name" json:"name"`
+	ID   bson.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Name string        `bson:"name" json:"name" binding:"required"`
 }
