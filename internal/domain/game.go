e71a3ab21368deeb18c84525ad9afa64ec73ae08
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-31 14:30:29 +0500

    refactor: make original system and category fields embedded

--- internal/domain/game.go
+++ internal/domain/game.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Game struct {
-	ID               bson.ObjectID `bson:"_id,omitempty" json:"_id"`
-	PublisherId      bson.ObjectID `bson:"publisher_id" json:"publisher_id"`
-	DeveloperId      bson.ObjectID `bson:"developer_id" json:"developer_id"`
-	OriginalSystemId bson.ObjectID `bson:"original_system_id" json:"original_system_id"`
-	EmulationId      bson.ObjectID `bson:"emulation_id" json:"emulation_id"`
-	Title            string        `bson:"title" json:"title"`
-	Description      string        `bson:"description" json:"description"`
-	ReleaseDate      time.Time     `bson:"release_date" json:"release_date"`
-	Price            float32       `bson:"price" json:"price"`
-	CreatedAt        time.Time     `bson:"created_at" json:"created_at"`
-	UpdatedAt        time.Time     `bson:"updated_at" json:"updated_at"`
-	Category         bson.ObjectID `bson:"category" json:"category"`
-	IsVerified       bool          `bson:"is_verified" json:"is_verified"`
+	ID             bson.ObjectID `bson:"_id,omitempty" json:"_id"`
+	PublisherId    bson.ObjectID `bson:"publisher_id" json:"publisher_id"`
+	DeveloperId    bson.ObjectID `bson:"developer_id" json:"developer_id"`
+	EmulationId    bson.ObjectID `bson:"emulation_id" json:"emulation_id"`
+	OriginalSystem string        `bson:"original_system" json:"original_system"`
+	Title          string        `bson:"title" json:"title"`
+	Description    string        `bson:"description" json:"description"`
+	ReleaseDate    time.Time     `bson:"release_date" json:"release_date"`
+	Price          float32       `bson:"price" json:"price"`
+	IsVerified     bool          `bson:"is_verified" json:"is_verified"`
+	Category       []string      `bson:"category" json:"category"`
+	CreatedAt      time.Time     `bson:"created_at" json:"created_at"`
+	UpdatedAt      time.Time     `bson:"updated_at" json:"updated_at"`
 }
