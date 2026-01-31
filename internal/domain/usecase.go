b71246339fee6749e4ffa8f597304fe2e0428a1f
Animantra <t20227893@gmail.com>
2026-01-31 19:11:14 +0500

    feat: add usecase interfaces for company and emulation

--- internal/domain/usecase.go
+++ internal/domain/usecase.go
@@ -9,3 +9,19 @@ type GameUC interface {
 	Update(ctx context.Context, id string, updates *Game) error
 	Delete(ctx context.Context, id string) error
 }
+
+type CompanyUC interface {
+	Create(ctx context.Context, companies *Company) error
+	GetAll(ctx context.Context) ([]*Company, error)
+	GetById(ctx context.Context, id string) (*Company, error)
+	Update(ctx context.Context, id string, updates *Company) error
+	Delete(ctx context.Context, id string) error
+}
+
+type EmulationUC interface {
+	Create(ctx context.Context, emulations *Emulation) error
+	GetAll(ctx context.Context) ([]*Emulation, error)
+	GetById(ctx context.Context, id string) (*Emulation, error)
+	Update(ctx context.Context, id string, updates *Emulation) error
+	Delete(ctx context.Context, id string) error
+}
