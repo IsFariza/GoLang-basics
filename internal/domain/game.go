f0431e498311b289743360c24cfd124900f00a75
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-01-31 17:37:59 +0500

    fix: make title field required

--- internal/domain/game.go
+++ internal/domain/game.go
@@ -12,7 +12,7 @@ type Game struct {
 	DeveloperId    bson.ObjectID `bson:"developer_id" json:"developer_id"`
 	EmulationId    bson.ObjectID `bson:"emulation_id" json:"emulation_id"`
 	OriginalSystem string        `bson:"original_system" json:"original_system"`
-	Title          string        `bson:"title" json:"title"`
+	Title          string        `bson:"title" json:"title" binding:"required"`
 	Description    string        `bson:"description" json:"description"`
 	ReleaseDate    time.Time     `bson:"release_date" json:"release_date"`
 	Price          float32       `bson:"price" json:"price"`
