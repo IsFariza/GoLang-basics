0d88edf4005c2e98d3455115935c67a24f955373
IsFariza <fariza.ismag@gmail.com>
2026-02-01 22:17:41 +0500

    fix: add validation

--- internal/domain/review.go
+++ internal/domain/review.go
@@ -8,10 +8,10 @@ import (
 
 type Review struct {
 	ID        bson.ObjectID `bson:"_id,omitempty" json:"_id"`
-	UserId    bson.ObjectID `bson:"user_id,omitempty" json:"user_id"`
-	GameId    bson.ObjectID `bson:"game_id,omitempty" json:"game_id"`
-	Rating    float32       `bson:"rating" json:"rating"`
+	UserId    bson.ObjectID `bson:"user_id" json:"user_id" binding:"required"`
+	GameId    bson.ObjectID `bson:"game_id" json:"game_id" binding:"required"`
+	Rating    *float32      `bson:"rating" json:"rating" binding:"required"`
 	Content   string        `bson:"content" json:"content"`
 	CreatedAt time.Time     `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time     `bson:"updated_at" json:"updated_at"`
+	UpdatedAt *time.Time    `bson:"updated_at" json:"updated_at"`
 }
--- internal/domain/usecase.go
+++ internal/domain/usecase.go
@@ -6,6 +6,7 @@ type GameUC interface {
 	Create(ctx context.Context, game *Game) error
 	GetAll(ctx context.Context) ([]*Game, error)
 	GetById(ctx context.Context, id string) (*Game, error)
+	GetReviewsByGameId(ctx context.Context, id string) ([]*Review, error)
 	Update(ctx context.Context, id string, updates *Game) error
 	Delete(ctx context.Context, id string) error
 }
@@ -23,5 +24,20 @@ type EmulationUC interface {
 	GetAll(ctx context.Context) ([]*Emulation, error)
 	GetById(ctx context.Context, id string) (*Emulation, error)
 	Update(ctx context.Context, id string, updates *Emulation) error
+	Delete(ctx context.Context, id string) error
+}
+
+type ReviewUC interface {
+	Create(ctx context.Context, reviews *Review) error
+	GetAll(ctx context.Context) ([]*Review, error)
+	GetById(ctx context.Context, id string) (*Review, error)
+	Update(ctx context.Context, id string, updates *Review) error
+	Delete(ctx context.Context, id string) error
+}
+
+type PurchaseUC interface {
+	Create(ctx context.Context, purchase *Purchase) error
+	GetAll(ctx context.Context) ([]*Purchase, error)
+	GetById(ctx context.Context, id string) (*Purchase, error)
 	Delete(ctx context.Context, id string) error
 }
