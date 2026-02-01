7f318ade990ccf678e3996036acc6686cd7ef11d
Animantra <t20227893@gmail.com>
2026-02-01 21:39:33 +0500

    refactor: distribute routes by permissions

--- internal/delivery/http/routes.go
+++ internal/delivery/http/routes.go
@@ -3,49 +3,19 @@ package http
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(r *gin.Engine, gameH *GameHandler, companyH *CompanyHandler, emulationH *EmulationHandler, userH *UserHandler) {
-	// Base API Group
 	api := r.Group("/api/v1")
 	{
-		// Games API Group
-		games := api.Group("/games")
-		{
-			// games.POST("/", gameH.Create)
-			games.GET("/", gameH.GetAll)
-			games.GET("/:id", gameH.GetById)
-			// games.PUT("/:id", gameH.Update)
-			// games.DELETE("/:id", gameH.Delete)
-		}
-
-		companies := api.Group("/companies")
-		{
-			companies.POST("/", companyH.Create)
-			companies.GET("/", companyH.GetAll)
-			companies.GET("/:id", companyH.GetById)
-			companies.PUT("/:id", companyH.Update)
-			companies.DELETE("/:id", companyH.Delete)
-		}
-
-		emulations := api.Group("/emulations")
-		{
-			emulations.POST("/", emulationH.Create)
-			emulations.GET("/", emulationH.GetAll)
-			emulations.GET("/:id", emulationH.GetById)
-			emulations.PUT("/:id", emulationH.Update)
-			emulations.DELETE("/:id", emulationH.Delete)
-		}
-
-		users := api.Group("/users")
-		{
-			users.POST("/", userH.Create)
-			users.GET("/", userH.GetAll)
-			users.GET("/:id", userH.GetById)
-			users.PUT("/:id", userH.Update)
-			users.DELETE("/:id", userH.Delete)
-		}
-
 		api.POST("/signup", userH.SignUp)
 		api.POST("/login", userH.Login)
 
+		api.GET("/games", gameH.GetAll)
+		api.GET("/games/:id", gameH.GetById)
+		api.GET("/companies", companyH.GetAll)
+		api.GET("/companies/:id", companyH.GetById)
+		api.GET("/emulations", emulationH.GetAll)
+		api.GET("/emulations/:id", emulationH.GetById)
+
+		// Only for users
 		auth := api.Group("/", AuthMiddleware("user"))
 		{
 			auth.POST("/games", gameH.Create)
@@ -53,12 +23,17 @@ func RegisterRoutes(r *gin.Engine, gameH *GameHandler, companyH *CompanyHandler,
 			auth.DELETE("/games/:id", gameH.Delete)
 
 			auth.POST("/companies", companyH.Create)
-			auth.POST("/emulations", emulationH.Create)
+			auth.PUT("/companies/:id", companyH.Update)
+			auth.DELETE("/companies/:id", companyH.Delete)
 
+			auth.POST("/emulations", emulationH.Create)
+			auth.PUT("/emulations/:id", emulationH.Update)
+			auth.DELETE("/emulations/:id", emulationH.Delete)
+
+			// Only for admins
 			admin := auth.Group("/admin", AuthMiddleware("admin"))
 			{
 				admin.PATCH("/games/:id/approve", gameH.Approve)
-
 				admin.GET("/users", userH.GetAll)
 				admin.DELETE("/users/:id", userH.Delete)
 			}
