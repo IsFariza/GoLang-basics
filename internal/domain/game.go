b19f6d7dada0094e5bb378d224202638a4a4eecc
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-02 00:56:48 +0500

    feat: add PopulatedGame model

--- internal/domain/game.go
+++ internal/domain/game.go
@@ -21,3 +21,19 @@ type Game struct {
 	CreatedAt      time.Time     `bson:"created_at" json:"created_at"`
 	UpdatedAt      *time.Time    `bson:"updated_at" json:"updated_at"`
 }
+
+type PopulatedGame struct {
+	ID             bson.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Publisher      *Company      `bson:"publisher" json:"publisher" binding:"required"`
+	Developer      *Company      `bson:"developer" json:"developer" binding:"required"`
+	Emulation      *Emulation    `bson:"emulation" json:"emulation"`
+	OriginalSystem string        `bson:"original_system" json:"original_system" binding:"omitempty,min=1,max=250"`
+	Title          string        `bson:"title" json:"title" binding:"required,min=2,max=100"`
+	Description    string        `bson:"description" json:"description" binding:"max=1000"`
+	ReleaseDate    *time.Time    `bson:"release_date" json:"release_date"`
+	Price          *float32      `bson:"price" json:"price" binding:"required,min=0"`
+	IsVerified     bool          `bson:"is_verified" json:"is_verified"`
+	Category       []string      `bson:"category" json:"category" binding:"omitempty,min=1,dive,min=1"`
+	CreatedAt      time.Time     `bson:"created_at" json:"created_at"`
+	UpdatedAt      *time.Time    `bson:"updated_at" json:"updated_at"`
+}
