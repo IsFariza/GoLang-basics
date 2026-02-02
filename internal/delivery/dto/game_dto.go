bd85f8c7659ba85833fd0f7d68f184d680a6fbfc
Meirhan Syzdykov <syzdykovmr@gmail.com>
2026-02-02 13:48:22 +0500

    fix: change required to omitempty

--- internal/delivery/dto/game_dto.go
+++ internal/delivery/dto/game_dto.go
@@ -5,14 +5,14 @@ import (
 )
 
 type UpdateGameRequest struct {
-	PublisherId    *string    `bson:"publisher_id" json:"publisher_id" binding:"required"`
-	DeveloperId    *string    `bson:"developer_id" json:"developer_id" binding:"required"`
-	EmulationId    *string    `bson:"emulation_id" json:"emulation_id"`
-	OriginalSystem *string    `bson:"original_system" json:"original_system" binding:"omitempty,min=1,max=250"`
-	Title          *string    `bson:"title" json:"title" binding:"required,min=2,max=100"`
-	Description    *string    `bson:"description" json:"description" binding:"max=1000"`
-	ReleaseDate    *time.Time `bson:"release_date" json:"release_date"`
-	Price          *float32   `bson:"price" json:"price" binding:"required,min=0"`
-	Category       []string   `bson:"category" json:"category" binding:"omitempty,min=1,dive,min=1"`
-	UpdatedAt      *time.Time `bson:"updated_at" json:"updated_at"`
+	PublisherId    *string    `json:"publisher_id"`
+	DeveloperId    *string    `json:"developer_id"`
+	EmulationId    *string    `json:"emulation_id"`
+	OriginalSystem *string    `json:"original_system" binding:"omitempty,min=1,max=250"`
+	Title          *string    `json:"title" binding:"omitempty,min=2,max=100"`
+	Description    *string    `json:"description" binding:"max=1000"`
+	ReleaseDate    *time.Time `json:"release_date"`
+	Price          *float32   `json:"price" binding:"omitempty,min=0"`
+	Category       []string   `json:"category" binding:"omitempty,min=1,dive,min=1"`
+	UpdatedAt      *time.Time `json:"updated_at"`
 }
