264515c9d5d882ce74b2c12f632a24bf418f9b4c
IsFariza <fariza.ismag@gmail.com>
2026-02-05 01:38:42 +0500

    feat: add Username field into Review model

--- internal/domain/review.go
+++ internal/domain/review.go
@@ -8,9 +8,10 @@ import (
 
 type Review struct {
 	ID        bson.ObjectID `bson:"_id,omitempty" json:"_id"`
-	UserId    bson.ObjectID `bson:"user_id" json:"user_id" binding:"required"`
-	GameId    bson.ObjectID `bson:"game_id" json:"game_id" binding:"required"`
-	Rating    *float32      `bson:"rating" json:"rating" binding:"required"`
+	UserId    bson.ObjectID `bson:"user_id" json:"user_id"`
+	Username  string        `bson:"username" json:"username"`
+	GameId    bson.ObjectID `bson:"game_id" json:"game_id"`
+	Rating    *int          `bson:"rating" json:"rating" binding:"required"`
 	Content   string        `bson:"content" json:"content"`
 	CreatedAt time.Time     `bson:"created_at" json:"created_at"`
 	UpdatedAt *time.Time    `bson:"updated_at" json:"updated_at"`
